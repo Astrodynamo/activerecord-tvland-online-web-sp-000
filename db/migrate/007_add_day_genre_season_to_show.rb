f86ff44ecb6e91a97f75f362724980def0380990
Amanda King <amapickell@gmail.com>
2020-06-07 16:37:27 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_genre_season_to_show.rb
+++ db/migrate/007_add_day_genre_season_to_show.rb
@@ -2,6 +2,8 @@ class AddDayGenreSeasonToShow < ActiveRecord::Migration[5.1]
   
   def change
     add_column :show, :day, :string
+    add_column :show, :genre, :string
+    add_column :show, :season, :string
   end
   
 end
